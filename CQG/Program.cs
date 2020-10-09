7c234e397718dbc19f1243226610bc7e81574d23
Roman <sherlock.h.90@mail.ru>
2020-10-09 09:51:52 +0300

    initialize commit

---
+++ CQG/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace CQG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (count != 2)
+            {
+                string AllWords = Console.ReadLine();
+                if (AllWords == "===")
+                {
+                    count++;
+                }
+            }
+        }
+
+        static void SplitRows(string str)
+        {
+
+        }
+    }
+}
