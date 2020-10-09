7a93cd58f8df74a0eca212a1a74497bdd18c8da0
Roman <sherlock.h.90@mail.ru>
2020-10-09 11:53:30 +0300

    написал часть основных методов и сделал класс элемента слова.

--- CQG/Program.cs
+++ CQG/Program.cs
@@ -1,26 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace CQG
 {
     class Program
     {
+        static List<string> Dict = new List<string>();
+        static List<WordElement> Content = new List<WordElement>();
+
         static void Main(string[] args)
         {
             int count = 0;
+            List<string> inputData = new List<string>();
             while (count != 2)
             {
-                string AllWords = Console.ReadLine();
-                if (AllWords == "===")
+                var str = Console.ReadLine();
+                if (str == "===")
                 {
                     count++;
+                }
+                inputData.Add(str);
+            }
+            SplitRows(inputData);
+            foreach (var wordContent in Content)
+            {
+                FindWord(wordContent);
+            }
+        }
+
+        static void SplitRows(List<string> str)
+        {
+            bool content = false; ;
+            foreach (var i in str)
+            {
+                if (i == "===")
+                {
+                    content = true;
+                    continue;
+                }
+                if (!content)
+                {
+                    foreach (var word in i.Split(' '))
+                    {
+                        Dict.Add(word);
+                    }
+                }
+                else
+                {
+                    foreach (var word in i.Split(' '))
+                    {
+                        WordElement tmp = new WordElement(i);
+                        Content.Add(tmp);
+                    }
                 }
             }
         }
 
-        static void SplitRows(string str)
+        static void FindWord(WordElement word)
         {
+            char[] errorW = word.value.ToCharArray();
+            foreach (var DictWord in Dict)
+            {
+                if (Math.Abs(DictWord.Length - word.value.Length) < 2)
+                {
+                    if (DictWord.Length > word.value.Length)
+                    { 
 
+                    }
+                }
+            }
         }
+
+        static void CompareWords(WordElement word) 
+        { 
+        
+        }
+
     }
 }
---
+++ CQG/WordElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQG
+{
+    public enum TypeOfError
+    {
+        exactly,
+        delete,
+        insert,
+        notFound,
+        ambiguity
+    }
+
+    public class WordElement
+    {
+        public string value { get; set; }
+        public TypeOfError type { get; set; }
+        public int countOfMatches = 0;
+        public WordElement(string _value)
+        {
+            value = _value;
+        }
+
+    }
+}
